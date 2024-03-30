585ccc8bbe918594319f5b3c06332b41f8052c95
20521319 <20521319@uit.edu.vn>
2024-03-30 19:34:37 +0700

    register

--- server/restaurant_be/api/serializers.py
+++ server/restaurant_be/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'phone']
+        fields = ['id', 'email', 'password', 'phone', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
--- server/restaurant_be/api/views.py
+++ server/restaurant_be/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import check_password, make_password
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.validated_data['password'] = make_password(serializer.validated_data['password'])
+    user = serializer.save()
+    return Response({
+        'status': 'success',
+        'data':{
+            'id': user.id,
+            'email': user.email,
+            'phone': user.phone,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        }})
